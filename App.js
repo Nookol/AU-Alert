fc9ebb4b6bf5654e4939e4cfa1aa141b8c3cbd98
Narayan Chuwan <111250533+nchuwan01@users.noreply.github.com>
2024-02-09 18:02:31 -0600

    all

--- App.js
+++ App.js
@@ -1,20 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import MainMessage from "./Messaging/MainMessage";
+import SendIcon from './Messaging/SendIcon';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+        <MainMessage/>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  safeArea: {
+    height: "100%",
+    paddingTop: 40,
+    backgroundColor: "green"
+  }
+  
+
 });
+

716a721a032f535bfac2ef3a2531bdca637e98df
cvalencia02 <cvalencia02@aurora.edu>
2024-04-14 12:24:09 -0500

    Password Base
    hopefully password reset works

--- auth/firebase.js
+++ auth/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
+import { getAuth, sendPasswordResetEmail } from 'firebase/auth';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCYH0AX14R9G1TkADeJWxqZ6_aMEEWmamg",
@@ -10,6 +10,17 @@ const firebaseConfig = {
     appId: "1:81948261786:web:85b921f67d075122964e23",
     measurementId: "G-XL5YWLR8PZ"
 };
+
+const email = document.getElementById("email").value;
+sendPasswordResetEmail(auth, email)
+    .then(data => {
+    console.log(data);
+    alert("Email Sent")
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+})
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
